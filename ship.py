ab76897278e45d4426138592fdae70c191d513a0
Francisco Echeverria <fcontreras18@gmail.com>
2017-04-19 10:38:59 -0700

    Update ship, settings, game functions

--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialized the ship and set its starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/vape_naysh.bmp')
@@ -15,6 +16,23 @@ class Ship():
         # Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # UPdate rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location"""
