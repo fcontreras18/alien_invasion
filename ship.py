1e42a4a6b7cb7beb4bce805211dad87ceef21026
Francisco Echeverria <fcontreras18@gmail.com>
2017-04-23 23:21:02 -0700

    Update working game

--- ship.py
+++ ship.py
@@ -15,8 +15,8 @@ class Ship():
 
         # Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
 
+        self.rect.bottom = self.screen_rect.bottom
         # Store a decimal value for the ship's center
         self.center = float(self.rect.centerx)
 
